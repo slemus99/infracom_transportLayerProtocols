19163f92bec30ff97af0fa5a62e947d6b74bab61
Sebastián Lemus <sebastian.slc9@gmail.com>
2019-09-28 16:48:57 -0500

    Client Finished. File Transference Working

--- src/TCP/Client/Client.kt
+++ src/TCP/Client/Client.kt
@@ -1,11 +1,96 @@
 package TCP.Client
 
-import java.io.BufferedReader
-import java.io.PrintWriter
+import java.io.*
+import java.net.Socket
+
+/**
+ * READY: notifies that the server is ready to send a file (format: "READY <file_1>;<file_2>;...;<file_n>")
+ * SIZES: Message that the server sends containing the sizes of the available files in bytes (format: "SIZES <file_1_size>;<file_2_size>;...;<file_n_size>")
+ * SEND: signals the server to SEND a file (format: "SEND <file_id>")
+ * ERR: an error in the process or an unexpected message in the protocol
+ */
+enum class Protocol(val msg: String){
+    READY("READY"),
+    SIZES( "SIZES"),
+    SEND("SEND"),
+    ERR("ERROR")
+}
+
+val filesPath = "./src/TCP/Client/data"
 
 data class Client(val port: Int, val server: String){
 
-    private fun prepareToRequest(br: BufferedReader, pw: PrintWriter){
+    val socket = Socket(server, port)
 
+    // Form Standard Input
+    val stdIn = BufferedReader(InputStreamReader(System.`in`))
+
+    public fun requestFile(){
+
+        println("Connected to Server: $socket")
+
+        // Streams used to receive the file
+        val ins = socket.getInputStream()
+
+        // Streams used to send messages
+        val br = BufferedReader(InputStreamReader(ins))
+        val pw = PrintWriter(socket.getOutputStream(), true)
+
+        // Files available in the server
+        val serverReadyMsg = br.readLine().split(" ")
+        val serverSizesMsg = br.readLine().split(" ")
+
+        if (serverReadyMsg[0] != Protocol.READY.msg || serverSizesMsg[0] != Protocol.SIZES.msg){
+
+            pw.println(Protocol.ERR.msg)
+        }else{
+
+            val fns = serverReadyMsg[1].split(";")
+            val fss = serverSizesMsg[1].split(";")
+            showFilesFromConsole(fns, fss)
+            val fileId = selectFileFromConsole()
+
+            val bos = BufferedOutputStream(FileOutputStream("$filesPath/${fns[fileId - 1]}"))
+
+            pw.println("${Protocol.SEND.msg} $fileId")
+
+            // receive and save the file
+            receiveFile(bos, ins)
+
+            bos.flush()
+        }
+
+        socket.close()
     }
+
+    private fun receiveFile(bos: BufferedOutputStream, ins: InputStream){
+        val contents = ByteArray(10000)
+        var curr = ins.read(contents)
+
+        val before = System.currentTimeMillis()
+        while (curr != -1){
+            bos.write(contents, 0, curr)
+            curr = ins.read(contents)
+        }
+        val after = System.currentTimeMillis()
+
+        println("Process finished successfully in ${after - before} ms")
+    }
+
+    private fun showFilesFromConsole(filesNames: List<String>, filesSizes: List<String>){
+
+        for (i in 0 until filesNames.size){
+            println("${i+1}) File Name: ${filesNames[i]} | File Size: ${filesSizes[i]}")
+        }
+    }
+
+    private fun selectFileFromConsole(): Int{
+        println("Select one of the above files typing its number (id): ")
+        return stdIn.readLine().toInt()
+    }
+}
+
+fun main(args: Array<String>) {
+    val c = Client(3400, "localhost")
+    c.requestFile()
 }
--- src/TCP/Server/Server.kt
+++ src/TCP/Server/Server.kt
@@ -6,11 +6,13 @@ import java.net.Socket
 
 /**
  * READY: notifies that the server is ready to send a file (format: "READY <file_1>;<file_2>;...;<file_n>")
- * SEND: signals the server to SEND a file (format: "SEND <file_name>")
+ * SIZES: Message that the server sends containing the sizes of the available files in bytes (format: "SIZES <file_1_size>;<file_2_size>;...;<file_n_size>")
+ * SEND: signals the server to SEND a file (format: "SEND <file_id>")
  * ERR: an error in the process or an unexpected message in the protocol
  */
 enum class Protocol(val msg: String){
     READY("READY"),
+    SIZES( "SIZES"),
     SEND("SEND"),
     ERR("ERROR")
 }
@@ -18,9 +20,21 @@ enum class Protocol(val msg: String){
 val filesPath = "./src/TCP/Server/data"
 
 /**
- * Based on an implementation from http://www.codebytes.in/2014/11/file-transfer-using-tcp-java.html
+ * Based on implementations from
+ * http://www.codebytes.in/2014/11/file-transfer-using-tcp-java.html
+ * https://stackoverflow.com/questions/9520911/java-sending-and-receiving-file-byte-over-sockets/
  */
 data class Server(val clientSocket: Socket, val id: Int): Thread(){
+
+
+    // List of available files in /data file
+    val files = File(filesPath).listFiles()
+
+    // A string containing the names of the files
+    val fileNames = getAvailableFileNames()
+
+    // A String containing the sizes of the files
+    val fileSizes = getAvailableFileSizes()
 
     override fun run() {
 
@@ -37,15 +51,15 @@ data class Server(val clientSocket: Socket, val id: Int): Thread(){
         val pw = PrintWriter(ops, true)
 
         // Extract the requested file by the client (while valid)
-        var fileName = prepareToSend(br, pw)
-        while (fileName != Protocol.ERR.msg){
-            fileName = prepareToSend(br, pw)
+        var fileId = prepareToSend(br, pw)
+        while (fileId == -1){
+            fileId = prepareToSend(br, pw)
         }
 
         // Sends the file
-        val file = File("$filesPath/$fileName")
+        val file = files[fileId - 1]
         val bisFile = BufferedInputStream(FileInputStream(file))
-        send(file, bisFile, bos)
+        sendFile(file, bisFile, bos)
 
         // Closes the socket and streams
         br.close()
@@ -59,46 +73,55 @@ data class Server(val clientSocket: Socket, val id: Int): Thread(){
      * Starts the communication with the client regarding the sending of the file.
      * Returns the requested file or a message with an error
      */
-    private fun prepareToSend(br: BufferedReader, pw: PrintWriter): String{
+    private fun prepareToSend(br: BufferedReader, pw: PrintWriter): Int{
 
-        // List of available files in /data file
-        val files = File(filesPath).listFiles()
-
-        val extractName: (File) -> String = {f: File ->
-            val path = f.name.split("/")
-            path[path.size - 1]
-        }
-        val filesNames = files.map { extractName(it) }
-        val availableFiles = filesNames.fold(""){x: String, y: String -> "$x;$y"}.drop(1)
-
-        // Notifies the client that the server is ready so send a f ile
-        pw.println("${Protocol.READY.msg} $availableFiles")
+        // Notifies the client that the server is ready so sendFile a file
+        pw.println("${Protocol.READY.msg} $fileNames")
+        pw.println("${Protocol.SIZES.msg} $fileSizes")
 
         // Awaits for the file that the user wants to get
         val ans = br.readLine().split(" ")
 
         if (ans[0] != Protocol.SEND.msg){
             pw.println(Protocol.ERR.msg)
-            return Protocol.ERR.msg
+            return -1
 
         }else{
-            val fileName = ans[1]
-            if (fileName in filesNames){
-                return fileName
+            val fileId = ans[1].toInt()
+            if (fileId > 0 && fileId <= files.size){
+                return fileId
             }else{
                 pw.println(Protocol.ERR.msg)
-                return Protocol.ERR.msg
+                return -1
             }
         }
 
 
     }
 
+    private fun getAvailableFileNames(): String{
+
+        val extractName: (File) -> String = {f: File ->
+            val path = f.name.split("/")
+            path[path.size - 1]
+        }
+
+        // Names of the files
+        val filesNames = files.map { extractName(it) }
+        return filesNames.fold(""){x: String, y: String -> "$x;$y"}.drop(1)
+    }
+
+    private fun getAvailableFileSizes(): String{
+
+        // sizes of the files in bytes
+        val fileSizes = files.map {it.length()}
+        return fileSizes.fold(""){x: String, y: Long -> "$x;$y"}.drop(1)
+    }
 
     /**
      * Sends a file to the client via its Output Stream
      */
-    private fun send(file: File, bis: BufferedInputStream, bos: BufferedOutputStream){
+    private fun sendFile(file: File, bis: BufferedInputStream, bos: BufferedOutputStream){
 
 
         println("The file selected by the client is: ${file.name}")
@@ -142,14 +165,29 @@ fun main(args: Array<String>) {
     println("Initializing server")
     val serverSocket = ServerSocket(3400)
 
-    val folder = File("./src/TCP/Server/data")
-    folder.listFiles().forEach(::println)
+//    val folder = File("./src/TCP/Server/data")
+//    folder.listFiles().forEach(::println)
+//
+//    val extractName: (File) -> String = {f: File ->
+//        val path = f.name.split("/")
+//        path[path.size - 1]
+//    }
+//
+//    val filesNames = folder.listFiles().map { extractName(it) }
+//    val availableFiles = filesNames.fold(""){x: String, y: String -> "$x;$y"}.drop(1)
+//    println(availableFiles)
+//
+//    val fileSizes = folder.listFiles().map {it.length()}
+//    val availableFileSizes = fileSizes.fold(""){x: String, y: Long -> "$x;$y"}.drop(1)
+//    println(availableFileSizes)
 
 
     while (true){
 
         // accept method blocks the current method until it accepts a client
+        println("Waiting for client")
         val clientSocket = serverSocket.accept()
+        print("Client Socket accepted: $clientSocket")
         val serverThread = Server(clientSocket, numThreads)
         serverThread.start()
 
